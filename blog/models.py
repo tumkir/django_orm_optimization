9c32531a6f96cbbcde6bbca1f384616ecdf16c56
tumkir <tumkir@gmail.com>
2021-01-23 04:10:58 +0300

    Order model inner classes and methods to match django coding style

--- blog/models.py
+++ blog/models.py
@@ -30,12 +30,16 @@ class Post(models.Model):
     slug = models.SlugField("Название в виде url", max_length=200)
     image = models.ImageField("Картинка")
     published_at = models.DateTimeField("Дата и время публикации")
-
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор", limit_choices_to={'is_staff': True})
     likes = models.ManyToManyField(User, related_name="liked_posts", verbose_name="Кто лайкнул", blank=True)
     tags = models.ManyToManyField("Tag", related_name="posts", verbose_name="Теги")
 
     objects = PostQuerySet.as_manager()
+
+    class Meta:
+        ordering = ['-published_at']
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
 
     def __str__(self):
         return self.title
@@ -43,43 +47,37 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', args={'slug': self.slug})
 
-    class Meta:
-        ordering = ['-published_at']
-        verbose_name = 'пост'
-        verbose_name_plural = 'посты'
-
 
 class Tag(models.Model):
     title = models.CharField("Тег", max_length=20, unique=True)
 
     objects = TagQuerySet.as_manager()
 
-    def __str__(self):
-        return self.title
-
-    def clean(self):
-        self.title = self.title.lower()
-
-    def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
-
     class Meta:
         ordering = ["title"]
         verbose_name = 'тег'
         verbose_name_plural = 'теги'
 
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag_filter', args={'tag_title': self.slug})
+
+    def clean(self):
+        self.title = self.title.lower()
+
 
 class Comment(models.Model):
     post = models.ForeignKey("Post", related_name="comments", on_delete=models.CASCADE, verbose_name="Пост, к которому написан")
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
-
     text = models.TextField("Текст комментария")
     published_at = models.DateTimeField("Дата и время публикации")
-
-    def __str__(self):
-        return f"{self.author.username} under {self.post.title}"
 
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f"{self.author.username} under {self.post.title}"
